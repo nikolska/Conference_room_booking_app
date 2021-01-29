6cf71dff6690b7b0ad3860cf79fe056784cbc275
nikolska <nikolska1995@gmail.com>
2021-01-29 15:28:35 +0100

    add modify and delete room views

--- conference_room_booking/conference_room_booking/urls.py
+++ conference_room_booking/conference_room_booking/urls.py
@@ -20,12 +20,11 @@ from main_app.views import *
 
 
 urlpatterns = [
-    path('', main_page_view, name='main_page'),
+    path('', rooms_list_view, name='rooms_list'),
     path('admin/', admin.site.urls, name='admin'),
     path('room/<int:room_id>', room_details_view, name='room_details'),
     path('/room/modify/<int:room_id>', room_modify_view, name='room_modify'),
     path('/room/delete/<int:room_id>', room_delete_view, name='room_delete'),
     path('/room/reserve/<int:room_id>', room_reserve_view, name='room_reserve'),
     path('room/new/', add_new_room, name='add_new_room'),
-    path('rooms/', rooms_list_view, name='rooms_list'),
 ]
--- conference_room_booking/main_app/views.py
+++ conference_room_booking/main_app/views.py
@@ -1,12 +1,8 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.urls import reverse
 
 from .models import Room
-
-
-def main_page_view(request):
-    return render(request, 'home_page.html')
 
 
 def rooms_list_view(request):
@@ -20,11 +16,44 @@ def room_details_view(request, room_id):
 
 
 def room_modify_view(request, room_id):
-    pass
+    room = get_object_or_404(Room, pk=room_id)
+    if request.method == 'GET':
+        ctx = {'room': room}
+        return render(request, 'room_modify.html', ctx)
+    elif request.method == 'POST':
+        name = request.POST.get('name')
+        capacity = request.POST.get('capacity')
+        projector = request.POST.get('projector')
+        if not name:
+            ctx = {'message': 'Name can not be empty!'}
+            return render(request, 'room_modify.html', ctx)
+        if name != room.name and Room.objects.filter(name=name):
+            ctx = {'message': 'Conference room with this name is already exist!'}
+            return render(request, 'room_modify.html', ctx)
+        if not capacity:
+            capacity = 0
+        if int(capacity) < 0:
+            ctx = {'message': 'Room capacity must not be less than zero!'}
+            return render(request, 'room_modify.html', ctx)
+        if projector == 'on':
+            projector = True
+        else:
+            projector = False
+        room.name = name
+        room.capacity = capacity
+        room.projector = projector
+        room.save()
+        return HttpResponseRedirect(reverse('rooms_list'))
 
 
 def room_delete_view(request, room_id):
-    pass
+    room = get_object_or_404(Room, pk=room_id)
+    if request.method == 'GET':
+        ctx = {'room': room}
+        return render(request, 'delete_room.html', ctx)
+    elif request.method == 'POST':
+        room.delete()
+        return HttpResponseRedirect(reverse('rooms_list'))
 
 
 def room_reserve_view(request, room_id):
